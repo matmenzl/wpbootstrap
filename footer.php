92aa1d2028d0778850190c6e9e4f07af7dfd94a7
matmenzl <info@mathiasmenzl.ch>
2016-06-24 22:48:43 +0200

    footer finished

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 </div> <!-- /container -->
 
 <footer>
-
+<h4>Die Raketebar ist Gemeinschaftsprojekte von</h4>
 <div class="specialBackground">
   <div class="container">
-    <a href="http://www.gz-zh.ch/gz-bachwiesen/gz-bachwiesen/" target="_blank"><img src="wp-content/themes/wpbootstrap/css/images/gzbachwiesen-breit.png"  border="0" alt="GZ Bachwiesen"></a>
-    <a href="http://www.hochneun.ch" target="_blank"><img src="wp-content/themes/wpbootstrap/css/images/hochneunlogo.png"  border="0" alt="Null"></a>
+    <a href="http://www.gz-zh.ch/gz-bachwiesen/gz-bachwiesen/" target="_blank"><img src="wp-content/themes/wpbootstrap/css/images/bachwiesen_transparent.png"  border="0" alt="GZ Bachwiesen"></a>
+    <a href="http://www.hochneun.ch" target="_blank"><img src="wp-content/themes/wpbootstrap/css/images/hochneun_transparent.png"  border="0" alt="Null"></a>
   </div>
 </div>
   <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y') ?></p>
