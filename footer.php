e3c34a66411067cb8287db58d01c63f8c38396cf
matmenzl <info@mathiasmenzl.ch>
2016-06-24 19:02:20 +0200

    hero image optimized and footer image and links added

--- footer.php
+++ footer.php
@@ -1,7 +1,17 @@
+</div> <!-- /container -->
+
 <footer>
+
+<div class="specialBackground">
+  <div class="container">
+    <a href="http://www.gz-zh.ch/gz-bachwiesen/gz-bachwiesen/" target="_blank"><img src="wp-content/themes/wpbootstrap/css/images/gzbachwiesen-breit.png"  border="0" alt="GZ Bachwiesen"></a>
+    <a href="http://www.hochneun.ch" target="_blank"><img src="wp-content/themes/wpbootstrap/css/images/hochneunlogo.png"  border="0" alt="Null"></a>
+  </div>
+</div>
   <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y') ?></p>
 </footer>
-</div> <!-- /container -->
+
+
 
 <?php wp_footer(); ?>
 </body>
